7c2f3f192543abe9f4fc77611d44903ff26b02c6
DirleBispo <dirleibispo@gmail.com>
2026-08-27 10:04:43 -0300

    Tornar itens do menu navegáveis

---
+++ menu-functional.js
@@ -0,0 +1,21 @@
+(function(){
+  const prev=window.showView;
+  function scrollPanel(){setTimeout(()=>document.querySelector('#contentPanel')?.scrollIntoView({behavior:'smooth',block:'start'}),80)}
+  function openMenu(){
+    if(typeof window.openPanel!=='function')return;
+    window.openPanel('Menu',`<div class="menu-action-list">
+      <button class="menu-action" onclick="showView('acessos')"><b>Administração</b><small>Cadastros, permissões e acessos da equipe.</small><span>›</span></button>
+      <button class="menu-action" onclick="showView('perfil')"><b>Minha conta</b><small>Login, perfil e acesso ao sistema.</small><span>›</span></button>
+      <button class="menu-action" onclick="showView('redes')"><b>Redes Sociais</b><small>Instagram, Facebook e WhatsApp oficiais da igreja.</small><span>›</span></button>
+      <button class="menu-action" onclick="showView('agenda')"><b>Agenda</b><small>Veja a programação e os próximos cultos.</small><span>›</span></button>
+      <button class="menu-action" onclick="showView('notificacoes')"><b>Avisos</b><small>Comunicados oficiais da igreja.</small><span>›</span></button>
+    </div>`);
+    scrollPanel();
+  }
+  window.showView=function(view){
+    if(view==='menu'){openMenu();return}
+    const r=prev(view);
+    if(view!=='inicio'&&view!=='falar')scrollPanel();
+    return r;
+  };
+})();
